d7e59da0d34ffc4320e82e232abebd731038d1ec
pirufio <francoandresvalle@gmail.com>
2016-07-08 23:39:32 -0300

    lalala

--- app/scripts/services/BuildList.js
+++ app/scripts/services/BuildList.js
@@ -11,34 +11,4 @@ angular.module('secondTestApp')
       }
     };
 
-
-  /*function BuildList(listData, BuildItem) {
-    this.BuildItem = BuildItem;
-    if (listData) {
-      this.setData(listData);
-    }
-
-  }
-
-  BuildList.prototype = {
-    setData: function(listData) {
-      console.info('set data');
-      angular.forEach(listData, function(value,key) {
-        console.info(key);
-        listData[key] = new BuildItem(value);
-        console.info(listData[key].status);
-      });
-      angular.extend(this, listData);
-    },
-    load: function() {
-      var scope = this;
-      console.info('load');
-      $http.get('/data/builds.json').success(function(data) {
-        console.info('get data');
-        scope.setData(data.data);
-      });
-    }
-  };
-  return BuildList;*/
-
 }]);
